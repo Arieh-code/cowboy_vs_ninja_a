865ed0bc236ad0ea6aaa0f6551d4c02e2cde6982
Arieh Norton <arieh.norton@gmail.com>
2023-05-20 16:36:40 +0300

    implemented team2

--- sources/team2.cpp
+++ sources/team2.cpp
@@ -1,31 +1,55 @@
 #include "team2.hpp"
 using namespace ariel;
 
-
-Team2::Team2(Character* teamLeader)
+Team2::Team2(Character *teamLeader)
     : Team(teamLeader)
-{this->add(teamLeader);}
-
-void Team2::add(Character* teamMember){
-
+{
+    this->add(teamLeader);
 }
 
-int Team2::stillAlive() {
-    return 0;
+void Team2::print()
+{
+    vector<Character *> &members = this->getTeamMembers();
+    for (auto &member : members)
+    {
+        cout << member->print() << endl;
+    }
 }
 
-void Team2::print(){
-    cout << "needs to be implemented" << endl;
+void Team2::attack(Team *enemyTeam)
+{
+    if (!this->getTeamLeader()->isAlive())
+    {
+        this->closesToLeader();
+    }
+    // get vector
+    vector<Character *> &members = this->getTeamMembers();
+    Character *victim = this->closestVictimToLeader(enemyTeam);
+    for (auto &member : members)
+    {
+        if (dynamic_cast<Cowboy *>(member))
+        {
+            Cowboy *cowboy = dynamic_cast<Cowboy *>(member);
+            cowboy->shoot(victim);
+        }
+        else if (dynamic_cast<Ninja *>(member))
+        {
+            Ninja *ninja = dynamic_cast<Ninja *>(member);
+            ninja->slash(victim);
+        }
+        if (!victim->isAlive())
+        {
+            victim = this->closestVictimToLeader(enemyTeam);
+        }
+    }
 }
 
-void Team2::attack(Team* enemyTeam){
-
-}
-
-void Team2::setNewLeader() {
-
-}
-
-void Team2::destructor(){
-    
+void Team2::destructor()
+{
+    vector<Character *> &members = this->getTeamMembers();
+    for (auto &member : members)
+    {
+        delete member;
+    }
+    members.clear();
 }
--- sources/team2.hpp
+++ sources/team2.hpp
@@ -6,17 +6,10 @@ namespace ariel
 {
     class Team2 : public Team
     {
-        vector<Character *> teamMembers;
-
     public:
         Team2(Character *);
-        void add(Character * character) override;
-        int stillAlive() override;
         void print() override;
         void attack(Team* enemyTeam) override;
-        void setNewLeader() override;
         void destructor() override;
-
-
     };
 }
