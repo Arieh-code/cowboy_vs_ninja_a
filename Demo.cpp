8d3cf25cfd5295e2ac2a9e19b719d42d137456f7
Arieh Norton <arieh.norton@gmail.com>
2023-05-23 22:12:02 +0300

    my demo override

--- Demo.cpp
+++ Demo.cpp
@@ -11,119 +11,159 @@
 #include <stdexcept>
 #include <cassert>
 using namespace std;
-
-#include "sources/Team.hpp" //no need for other includes
 #include "sources/Team2.hpp"
+#include "sources/Team.hpp" //no need for other includes
 
 using namespace ariel;
 
 int main()
 {
-   // Point a(32.3, 44), b(1.3, 3.5);
-   // assert(a.distance(b) == b.distance(a));
-   // Cowboy *tom = new Cowboy("Tom", a);
-   // OldNinja *sushi = new OldNinja("sushi", b);
-   // tom->shoot(sushi);
-   // cout << tom->print() << endl;
+   Point a(32.3, 44), b(1.3, 3.5);
+   assert(a.distance(b) == b.distance(a));
+   Cowboy *tom = new Cowboy("Tom", a);
+   OldNinja *sushi = new OldNinja("sushi", b);
+   tom->shoot(sushi);
+   cout << tom->print() << endl;
 
-   // sushi->move(tom);
-   // sushi->slash(tom);
+   sushi->move(tom);
+   sushi->slash(tom);
 
-   // Team team_A(tom);
-   // team_A.add(new YoungNinja("Yogi", Point(64, 57)));
+   Team team_A(tom);
+   team_A.add(new YoungNinja("Yogi", Point(64, 57)));
 
-   // // Team b(tom); should throw tom is already in team a
+   // Team b(tom); should throw tom is already in team a
 
-   // Team team_B(sushi);
-   // team_B.add(new TrainedNinja("Hikari", Point(12, 81)));
+   Team team_B(sushi);
+   team_B.add(new TrainedNinja("Hikari", Point(12, 81)));
 
-   // while (team_A.stillAlive() > 0 && team_B.stillAlive() > 0)
+   while (team_A.stillAlive() > 0 && team_B.stillAlive() > 0)
+   {
+      team_A.attack(&team_B);
+      team_B.attack(&team_A);
+      team_A.print();
+      team_B.print();
+   }
+
+   if (team_A.stillAlive() > 0)
+      cout << "winner is team_A" << endl;
+   else
+   {
+      cout << "winner is team_B" << endl;
+   }
+
+   // ------------------------ my demo ------------------------
+   // team 1
+   // Cowboy *teamLeader1 = new Cowboy("Team Leader", Point(-1, -1));
+   // YoungNinja *Yninja1 = new YoungNinja("Yninja1", Point(0, 0));
+   // OldNinja *oldYninja1 = new OldNinja("oldYninja1", Point(-0.5, -0.5));
+   // TrainedNinja *TrainedNinja1 = new TrainedNinja("TrainedNinja1", Point(0.5, 0.5));
+   // Cowboy *cowboy2 = new Cowboy("cowboy2", Point(-52.5, 39.6));
+   // Team team1(teamLeader1);
+   // team1.add(Yninja1);
+   // team1.add(oldYninja1);
+   // team1.add(TrainedNinja1);
+   // team1.add(cowboy2);
+
+   // cout << "printing team1 leader " << team1.getTeamLeader()->print() << endl; // printing the team leader
+   // cout << "printing team1" << endl;                                           // printing the team
+   // team1.print();                                                              // printing the team
+
+   // // team 2
+   // YoungNinja *Yninja2 = new YoungNinja("Yninja2", Point(0, 0));
+   // TrainedNinja *TrainedNinja2 = new TrainedNinja("TrainedNinja2", Point(1, 1));
+   // OldNinja *oldYninja2 = new OldNinja("oldYninja2", Point(2, 2));
+   // YoungNinja *Yninja3 = new YoungNinja("Yninja3", Point(3, 3));
+   // Cowboy *cowboy3 = new Cowboy("cowboy3", Point(-6, -6));
+   // Cowboy *cowboy4 = new Cowboy("cowboy4", Point(-7, -7));
+   // Cowboy *cowboy5 = new Cowboy("cowboy8", Point(-8, -8));
+   // Team team2(Yninja2);
+   // team2.add(TrainedNinja2);
+   // team2.add(oldYninja2);
+   // team2.add(Yninja3);
+   // team2.add(cowboy3);
+   // team2.add(cowboy4);
+   // team2.add(cowboy5);
+
+   // cout << "Printing team2 leader: " << team2.getTeamLeader()->print() << endl; // printing the team leader
+   // cout << "Printing team2" << endl;                                            //
+   // team2.print();
+
+   // // cout << "distance: " << Yninja1->distance(Yninja2) << endl;                         // printing the team
+
+   // for (int i = 0; i < 13; i++)
    // {
-   //    team_A.attack(&team_B);
-   //    team_B.attack(&team_A);
-   //    team_A.print();
-   //    team_B.print();
+   //    team1.attack(&team2);
+   //    cout << "Attack number: " << i + 1 << endl;
+   //    cout << "printing team1 leader: " << team1.getTeamLeader()->print() << endl;
+   //    cout << "printing team1" << endl;
+   //    team1.print(); // printing the team
+   //    cout << "\nprinting team2 leader: " << team2.getTeamLeader()->print() << endl;
+   //    cout << "printing team2\n"
+   //         << endl;
+   //    team2.print(); // printing the team
    // }
 
-   // if (team_A.stillAlive() > 0)
-   //    cout << "winner is team_A" << endl;
-   // else
-   //    cout << "winner is team_B" << endl;
+   // test team2
+   // Cowboy *teamLeader1 = new Cowboy("Team Leader1", Point(0, 0));
+   // Cowboy *teamMember1_2 = new Cowboy("Team member1_2", Point(-3, 0));
+   // Cowboy *teamMember1_3 = new Cowboy("Team member1_3", Point(5, 0));
+   // Cowboy *teamLeader2 = new Cowboy("Team Leader2", Point(1, 0));
+   // Cowboy *teamMember2_1 = new Cowboy("teamMember2_1", Point(-2, 0));
+   // Cowboy *teamMember2_2 = new Cowboy("teamMember2_2", Point(3, 0));
+   // Cowboy *teamMember2_3 = new Cowboy("teamMember2_3", Point(-5, 0));
+   // Team team1(teamLeader1);
+   // team1.add(teamMember1_2);
+   // team1.add(teamMember1_3);
+   // Team2 team2(teamLeader2);
+   // team2.add(teamMember2_1);
+   // team2.add(teamMember2_2);
+   // team2.add(teamMember2_3);
 
-   // ----------------------------------------
-   // ------------------- my demo to see everything is working ---------------------
-   // Point a(22, 22), b(33, 33), c(22.0, 22.0);
-   // cout << (a == b) << endl;
-   // cout << (a == c) << endl;
-   // cout << a.distance(b) << endl;
-   // Point d = a.moveTowards(a, b, 16);
-   // cout << d.print() << endl;
-   // Point e = a.moveTowards(a, b, 10);
-   // cout << e.print() << endl;
-   // ------------------------------
-   // ---------------------- Character checks --------------------------
+   // cout << "distance: " << teamLeader2->distance(teamMember2_1) << endl;
+   // cout << "distance: " << teamLeader2->distance(teamMember2_2) << endl;
+   // cout << "distance: " << teamLeader2->distance(teamMember2_3) << endl;
 
-   Point p1(10, 20), p2(15, 30);
-   // Character A(p1, 10, "Arieh");
-   // Character *B = new Character(p2, 100, "Sivan");
-   // Character dead(p1, 0, "BinLaden");
-   // cout << A.isAlive() << endl;
-   // cout << dead.isAlive() << endl;
-   // double distance = A.distance(B);
-   // cout << distance << endl;
-   // cout << dead.print() << endl;
-   // cout << B->print() << endl;
-   // cout << A.print() << endl;
-   // cout << A.getType().name() << endl;
-   // delete B;
+   // for (int i = 0; i < 4 ; i++){
+   //    team1.attack(&team2);
+   // }
 
-   //  ----------------- Cowboy check ---------------------
-   // Cowboy tom("tom", p1);
-   // Character *cowboy = new Cowboy("John", Point(0, 0));
-   // cout << tom.get_number_of_bullets() << endl;
-   // cout << tom.hasboolets() << endl;
-   // tom.shoot(cowboy);
-   // cout << cowboy->getHits() << endl;
-   // cout << tom.getHits() << endl;
-   // cout << cowboy->isAlive() << endl;
-   // cout << tom.getType().name() << endl;
-   // cout << cowboy->getType().name() << endl;
-   // cout << cowboy->print() << endl;
-   // cout << tom.print() << endl;
-   // tom.setHits(0);
-   // cowboy->setHits(0);
-   // cout << cowboy->print() << endl;
-   // cout << tom.print() << endl;
-   // delete cowboy;
+   // cout << "distance: " << teamLeader2->distance(teamMember2_1) << endl;
+   // cout << "distance: " << teamLeader2->distance(teamMember2_2) << endl;
+   // cout << "distance: " << teamLeader2->distance(teamMember2_3) << endl;
 
-   Point p3(20, 20);
-   // -------------------- Ninja testings ---------------------------
-   // Ninja ninja(p1, 100, "Random");
-   // cout << ninja.getSpeed() << endl;
-   // OldNinja oldNinja("Old Ninja", p2);
-   // TrainedNinja trainedNinja("Trained Ninja", p2);
-   // YoungNinja youngNinja("Young Ninja", p2);
-   // cout << ninja.getLocation().print() << endl;
-   // cout << oldNinja.getLocation().print() << endl;
-   // ninja.move(&oldNinja);
-   // oldNinja.move(&ninja);
-   // ninja.slash(&oldNinja);
-   // oldNinja.slash(&ninja);
-   // cout << ninja.getLocation().print() << endl;
-   // cout << oldNinja.getLocation().print() << endl;
-   // cout << "Ninja's speed: " << ninja.getSpeed() << std::endl;
-   // cout << "Old Ninja's speed: " << oldNinja.getSpeed() << std::endl;
-   // cout << "Trained Ninja's speed: " << trainedNinja.getSpeed() << std::endl;
-   // cout << "Young Ninja's speed: " << youngNinja.getSpeed() << std::endl;
+   // cout << "team leader1: " << team1.getTeamLeader()->print() << endl;
+   // cout << "printing team1" << endl;
+   // team1.print();
 
-   Cowboy* cowboy = new Cowboy("tom", p1);
-   Ninja* ninja = new OldNinja("old", p2);
+   // cout << "team leader2: " << team2.getTeamLeader()->print() << endl;
+   // cout << "printing team2" << endl;
+   // team2.print();
+   // for (int i = 0; i < 4; i++)
+   // {
+   //    team1.attack(&team2);
+   //    cout << "attack number: " << i + 1 << endl;
+   //    cout << "Printing team1" << endl;
+   //    team1.print();
+   //    cout << "\nprinting team2" << endl;
+   //    cout << "Team leader2: " << team2.getTeamLeader()->print() << "\n"
+   //         << endl;
+   //    team2.print();
+   // }
+   // cout << "********* end of first attack****************" << endl;
+   // cout << "\nTeam leader2: " << team2.getTeamLeader()->print() << endl;
 
-   Team team(cowboy);
-   Team2 team2(ninja);
-   Team team3(ninja);
-   cout << team.stillAlive() << endl;
-   cout << team2.stillAlive() << endl;
-   cout << team3.stillAlive() << endl;
+   // for (int i = 0; i < 6; i++)
+   // {
+   //    team2.attack(&team1);
+   //    cout << "attack number: " << i + 1 << endl;
+   //    cout << "\nTeam leader2: " << team2.getTeamLeader()->print() << endl;
+   //    cout << "Printing team2" << endl;
+   //    team2.print();
+   //    cout << "\nprinting team1" << endl;
+   //    cout << "Team leader1: " << team1.getTeamLeader()->print() << "\n"
+   //         << endl;
+   //    team1.print();
+   // }
+
    return 0; // no memory issues. Team should free the memory of its members. both a and b teams are on the stack.
 }
