a6a7a62b62c1e974c79ac66943f4fbe4b4687d66
Arieh Norton <arieh.norton@gmail.com>
2023-05-23 18:29:30 +0300

    updated cowboy logic

--- sources/Cowboy.cpp
+++ sources/Cowboy.cpp
@@ -18,6 +18,19 @@ void Cowboy::set_number_of_bullets(int bullets)
 
 void Cowboy::shoot(Character *enemy)
 {
+        if (enemy == this)
+    {
+        throw std::runtime_error("Can't shoot oneself");
+    }
+
+    if (!enemy->isAlive())
+    {
+        throw runtime_error("Can't shoo dead enemy");
+    }
+    if (!isAlive())
+    {
+        throw runtime_error("Can't shoot while being dead");
+    }
     if (this->isAlive() && number_of_bullets > 0)
     {
         enemy->hit(10);
@@ -36,5 +49,9 @@ bool Cowboy::hasboolets()
 
 void Cowboy::reload()
 {
+    if (!isAlive())
+    {
+        throw runtime_error("Dead cowboys can't reload");
+    }
     number_of_bullets = 6;
 }
